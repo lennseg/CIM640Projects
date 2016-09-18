791c66027a6e432010641d4fb0e99231708cf094
lennseg <lennseg@gmail.com>
2016-09-18 13:04:03 -0400

    new project

---
+++ array-of-circles/sketch.js
@@ -0,0 +1,22 @@
+var topBoundary = 0;
+var bottomBoundary = 0;
+
+var boundary0 = 0;
+var boundary1 = 0;
+var boundary2 = 0;
+var boundary3 = 0;
+
+var buttonSize = 100;
+
+var currentColor = "white";
+var brushSize = 10;
+
+var currentArea = "";
+
+function setup() {
+  createCanvas(500,500);
+}
+
+function draw() {
+  background(0);
+}
