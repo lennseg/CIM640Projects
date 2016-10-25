bf4480298065d753f0cf86dee25c6809aece4f77
lennseg <lennseg@gmail.com>
2016-10-25 17:40:18 -0400

    organized variables

--- halloween/sketch.js
+++ halloween/sketch.js
@@ -20,15 +20,28 @@ var y9 = 100;
 
 var buttonfill = 51;
 
+var centerBoxX = 123;
+var centerBoxY = 320;
+
+var centerBoxX2 = 323;
+var centerBoxY2 = 320;
+
+var centerBoxX3 = 515;
+var centerBoxY3 = 320;
+
+var allowDown = false;
+var lowDown = false;
+var weeDown = false;
+
 function setup() {
-  createCanvas(640,400);
+  createCanvas(640, 400);
   textFont("Georgia");
-} 
+}
 
 
 function draw() {
-  background(255,165,0);
-   
+  background(255, 165, 0);
+
   fill(0);
   textSize(42);
   text(oneChar, 100, y1);
@@ -40,53 +53,86 @@ function draw() {
   text(sevenChar, 410, y7);
   text(eightChar, 460, y8);
   text(nineChar, 510, y9);
-  
+
   fill(buttonfill);
-  rect(60, 300, 130, 40);
-  rect(260, 300, 130, 40);
-  rect(450, 300, 130, 40);
-  
+  rectMode(CENTER);
+  rect(centerBoxX, centerBoxY, 130, 40);
+  rect(centerBoxX2, centerBoxY2, 130, 40);
+  rect(centerBoxX3, centerBoxY3, 130, 40);
+
   fill(255);
   textSize(22);
   text("allow", 100, 329);
   text("low", 308, 329);
-  text("whoa", 490, 329);
+  text("wee", 498, 329);
+
 
 }
 
 // When the user clicks the mouse
 function mousePressed() {
+
   // Check if mouse is inside the button
-  var d = dist(mouseX, mouseY, 60, 300);
-  if (d < 150) { 
-    if (y2==200) {
-    y2 = 100;
-    y3 = 100;
-    y4 = 100;
-    y5 = 100;
-    y6 = 100;
-    }
-    
-    else {
-    y2 = 200;
-    y3 = 200;
-    y4 = 200;
-    y5 = 200;
-    y6 = 200;
+  var d = dist(mouseX, mouseY, centerBoxX, centerBoxY);
+  var d2 = dist(mouseX, mouseY, centerBoxX2, centerBoxY2);
+  var d3 = dist(mouseX, mouseY, centerBoxX3, centerBoxY3);
+  
+  if (d < 40) {
+    if (allowDown) {
+      allowDown = false;
+    } else {
+      allowDown = true;
     }
   }
-    var d2 = dist(mouseX, mouseY, 260, 300);
-  if (d2 < 150) { 
-    if (y4==200) {
-    y4 = 100;
-    y5 = 100;
-    y6 = 100;
-    }
-    
-    else {
-    y4 = 200;
-    y5 = 200;
-    y6 = 200;
+  
+  if (d2 < 40) {
+    if (lowDown) {
+      lowDown = false;
+    } else {
+      lowDown = true;
     }
   }
+  if (allowDown) {
+    setToAllow();
+  } else if (lowDown) {
+    setToLow();
+  } else {
+    setToNone();
+  }
+}
+
+function setToAllow() {
+  y1 = 100;
+  y2 = 200;
+  y3 = 200;
+  y4 = 200;
+  y5 = 200;
+  y6 = 200;
+  y7 = 100;
+  y8 = 100;
+  y9 = 100;
+}
+
+function setToLow() {
+  y1 = 100;
+  y2 = 100;
+  y3 = 100;
+  y4 = 200;
+  y5 = 200;
+  y6 = 200;
+  y7 = 100;
+  y8 = 100;
+  y9 = 100;
+}
+
+function setToNone() {
+  y1 = 100;
+  y2 = 100;
+  y3 = 100;
+  y4 = 100;
+  y5 = 100;
+  y6 = 100;
+  y7 = 100;
+  y8 = 100;
+  y9 = 100;
 }
