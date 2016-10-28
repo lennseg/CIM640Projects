0bb3881c81e69736c9f60e63a0a27f802128964d
lennseg <lennseg@gmail.com>
2016-10-27 22:52:01 -0400

    add array and loop to text

--- midterm/sketch.js
+++ midterm/sketch.js
@@ -1,15 +1,8 @@
 //Midterm: Halloween
 
-var char0;
-var char1;
-var char2;
-var char3;
-var char4;
-var char5;
-var char6;
-var char7;
-var char8;
+var letters = ["H", "A", "L", "L", "O", "W", "E", "E", "N"];
 
+var y0 = 100;
 var y1 = 100;
 var y2 = 100;
 var y3 = 100;
@@ -18,12 +11,14 @@ var y5 = 100;
 var y6 = 100;
 var y7 = 100;
 var y8 = 100;
-var y9 = 100;
 
 var buttonfill = 0;
 
-var centerBoxX = 123;
-var centerBoxY = 320;
+var buttonWidth = 130;
+var buttonHeight = 40;
+
+var centerBoxX1 = 123;
+var centerBoxY1 = 320;
 
 var centerBoxX2 = 323;
 var centerBoxY2 = 320;
@@ -37,53 +32,54 @@ var weeDown = false;
 
 
 function setup() {
-  createCanvas(640, 400);
+  createCanvas(630,390);
   textFont("Georgia");
+  textLeading(30);
   bg = loadImage("images/ghosts.jpg");
 }
-
 
 function draw() {
   background(bg);
 
-  fill(229,146,19);
+  fill(229, 146, 19);
   noStroke();
   textSize(42);
-  text(oneChar, 70, y1);
-  text(twoChar, 130, y2);
-  text(threeChar, 186, y3);
-  text(fourChar, 243, y4);
-  text(fiveChar, 298, y5);
-  text(sixChar, 358, y6);
-  text(sevenChar, 430, y7);
-  text(eightChar, 485, y8);
-  text(nineChar, 540, y9);
+  text(letters[0], 60, y0);
+  text(letters[1], 120, y1);
+  text(letters[2], 180, y2);
+  text(letters[3], 240, y3);
+  text(letters[4], 300, y4);
+  text(letters[5], 360, y5);
+  text(letters[6], 430, y6);
+  text(letters[7], 485, y7);
+  text(letters[8], 540, y8);
+
 
   fill(buttonfill);
   strokeWeight(1);
-  stroke(229,146,19);
+  stroke(229, 146, 19);
   rectMode(CENTER);
-  rect(centerBoxX, centerBoxY, 130, 40);
-  rect(centerBoxX2, centerBoxY2, 130, 40);
-  rect(centerBoxX3, centerBoxY3, 130, 40);
+  rect(centerBoxX1, centerBoxY1, buttonWidth, buttonHeight);
+  rect(centerBoxX2, centerBoxY2, buttonWidth, buttonHeight);
+  rect(centerBoxX3, centerBoxY3, buttonWidth, buttonHeight);
 
-  fill(229,146,19);
+  fill(229, 146, 19);
   noStroke();
   textSize(22);
   text("allow", 100, 329);
   text("low", 308, 329);
-  text("wee", 498, 329);
+  text("wee", 499, 329);
 }
 
 
 function mousePressed() {
 
   // Check if mouse is inside the button
-  var d = dist(mouseX, mouseY, centerBoxX, centerBoxY);
+  var d1 = dist(mouseX, mouseY, centerBoxX1, centerBoxY1);
   var d2 = dist(mouseX, mouseY, centerBoxX2, centerBoxY2);
   var d3 = dist(mouseX, mouseY, centerBoxX3, centerBoxY3);
 
-  if (d < 40) {
+  if (d1 < 40) {
     if (allowDown) {
       allowDown = false;
     } else {
@@ -117,39 +113,39 @@ function mousePressed() {
 }
 
 function setToAllow() {
-  y1 = 100;
+  y0 = 100;
+  y1 = 200;
   y2 = 200;
   y3 = 200;
   y4 = 200;
   y5 = 200;
-  y6 = 200;
+  y6 = 100;
   y7 = 100;
   y8 = 100;
-  y9 = 100;
 }
 
 function setToLow() {
+  y0 = 100;
   y1 = 100;
   y2 = 100;
-  y3 = 100;
+  y3 = 200;
   y4 = 200;
   y5 = 200;
-  y6 = 200;
+  y6 = 100;
   y7 = 100;
   y8 = 100;
-  y9 = 100;
 }
 
 function setToWee() {
+  y0 = 100;
   y1 = 100;
   y2 = 100;
   y3 = 100;
   y4 = 100;
-  y5 = 100;
+  y5 = 200;
   y6 = 200;
   y7 = 200;
-  y8 = 200;
-  y9 = 100;
+  y8 = 100;
 }
 
 function setToNone() {
