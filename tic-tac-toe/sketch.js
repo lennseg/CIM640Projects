571a3e471c4736a956484df18aa26c8d2214a9fd
lennseg <lennseg@gmail.com>
2016-11-30 14:32:51 -0500

    fixing the code with seven

--- tic-tac-toe/sketch.js
+++ tic-tac-toe/sketch.js
@@ -43,30 +43,84 @@ function draw() {
   stroke(255);
   strokeWeight(2);
   rectMode(CENTER);
-  scribble.scribbleRect( centerBoxX1, centerBoxY1, boxWidth, boxHeight ); //top left box
-  scribble.scribbleRect( centerBoxX2, centerBoxY2, boxWidth, boxHeight ); //top middle box
-  scribble.scribbleRect( centerBoxX3, centerBoxY3, boxWidth, boxHeight ); //top right box
-  scribble.scribbleRect( centerBoxX4, centerBoxY4, boxWidth, boxHeight ); //middle left box
-  scribble.scribbleRect( centerBoxX5, centerBoxY5, boxWidth, boxHeight ); //middle box
-  scribble.scribbleRect( centerBoxX6, centerBoxY6, boxWidth, boxHeight ); //middle right box
-  scribble.scribbleRect( centerBoxX7, centerBoxY7, boxWidth, boxHeight ); //bottom left box
-  scribble.scribbleRect( centerBoxX8, centerBoxY8, boxWidth, boxHeight ); //bottom middle box
-  scribble.scribbleRect( centerBoxX9, centerBoxY9, boxWidth, boxHeight ); //bottom right box
+  //top left box
+  scribble.scribbleRect( centerBoxX1, centerBoxY1, boxWidth, boxHeight );
+  //top middle box
+  scribble.scribbleRect( centerBoxX2, centerBoxY2, boxWidth, boxHeight );
+  //top right box
+  scribble.scribbleRect( centerBoxX3, centerBoxY3, boxWidth, boxHeight );
+  //middle left box
+  scribble.scribbleRect( centerBoxX4, centerBoxY4, boxWidth, boxHeight );
+  //middle box
+  scribble.scribbleRect( centerBoxX5, centerBoxY5, boxWidth, boxHeight );
+  //middle right box
+  scribble.scribbleRect( centerBoxX6, centerBoxY6, boxWidth, boxHeight );
+  //bottom left box
+  scribble.scribbleRect( centerBoxX7, centerBoxY7, boxWidth, boxHeight );
+  //bottom middle box
+  scribble.scribbleRect( centerBoxX8, centerBoxY8, boxWidth, boxHeight );
+  //bottom right box
+  scribble.scribbleRect( centerBoxX9, centerBoxY9, boxWidth, boxHeight );
 
 }
 
 function mouseClicked (){
-  // Check if mouse is inside the bux
-  var d1 = dist(mouseX, mouseY, centerBoxX1, centerBoxY1);  //if (d1 < boxWidth) {console.log("it is in box 1!")};
-  var d2 = dist(mouseX, mouseY, centerBoxX2, centerBoxY2);  //if (d2 < boxWidth) {console.log("it is in box 2!")};
-  var d3 = dist(mouseX, mouseY, centerBoxX3, centerBoxY3);  //if (d3 < boxWidth) {console.log("it is in box 3!")};
+  // Check if mouse is inside the box
+  var d1 = dist(mouseX, mouseY, centerBoxX1, centerBoxY1); 
+  console.log(d1);
+  if (d1 < boxWidth/2) {
+    console.log("it is in box 1!"); 
+    // gridArray[0] = currentPlayer;
+    };
   
-  var d4 = dist(mouseX, mouseY, centerBoxX4, centerBoxY4);  //if (d4 < boxWidth) {console.log("it is in box 4!")};
-  var d5 = dist(mouseX, mouseY, centerBoxX5, centerBoxY5);  //if (d5 < boxWidth) {console.log("it is in box 5!")};
-  var d6 = dist(mouseX, mouseY, centerBoxX6, centerBoxY6);  //if (d6 < boxWidth) {console.log("it is in box 6!")};
+  var d2 = dist(mouseX, mouseY, centerBoxX2, centerBoxY2);
+  console.log(d2);
+  if (d2 < boxWidth/2){
+    console.log("it is in box 2!")
+  }
   
-  var d7 = dist(mouseX, mouseY, centerBoxX7, centerBoxY7);  //if (d7 < boxWidth) {console.log("it is in box 7!")};
-  var d8 = dist(mouseX, mouseY, centerBoxX8, centerBoxY8);  //if (d8 < boxWidth) {console.log("it is in box 8!")};
-  var d9 = dist(mouseX, mouseY, centerBoxX9, centerBoxY9);  //if (d9 < boxWidth) {console.log("it is in box 9!")};
+  var d3 = dist(mouseX, mouseY, centerBoxX3, centerBoxY3);
+  console.log(d3);
+  if (d3 < boxWidth/2){
+    console.log("it is in box 3!")
+  }
   
+  var d4 = dist(mouseX, mouseY, centerBoxX4, centerBoxY4);
+  console.log(d4);
+  if (d4 < boxWidth/2){
+    console.log("it is in box 4!")
+  }
+  
+  var d5 = dist(mouseX, mouseY, centerBoxX5, centerBoxY5);
+  console.log(d5);
+  if (d5 < boxWidth/2){
+    console.log("it is in box 5!")
+  }
+  
+  var d6 = dist(mouseX, mouseY, centerBoxX6, centerBoxY6);
+  console.log(d6);
+  if (d6 < boxWidth/2){
+    console.log("it is in box 6!")
+  }
+  
+  var d7 = dist(mouseX, mouseY, centerBoxX7, centerBoxY7);
+  console.log(d7);
+  if (d7 < boxWidth/2){
+    console.log("it is in box 7!")
+  }
+  
+  var d8 = dist(mouseX, mouseY, centerBoxX8, centerBoxY8);
+  console.log(d8);
+  if (d8 < boxWidth/2){
+    console.log("it is in box 8!")
+  }
+  
+  var d9 = dist(mouseX, mouseY, centerBoxX9, centerBoxY9);
+  console.log(d9);
+  if (d9 < boxWidth/2){
+    console.log("it is in box 9!")
+  }
+  
+ //check all possibilities
+ 
 }
